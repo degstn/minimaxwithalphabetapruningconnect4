fd75ed95b3446b7736e5e10c6ae3151647b9ec8d
drew goldstein <drew@degstn.com>
2025-10-16 10:05:21 -0400

    Co-authored-by: clara <clairetheflare@users.noreply.github.com> Co-authored-by: sarah-morales <sarah-morales@users.noreply.github.com> Co-authored-by: user.jjonespc <jjonespc@users.noreply.github.com>

--- main.py
+++ main.py
@@ -11,21 +11,29 @@ class Board:
         print(self.grid)
         
     def placeToken(self, x):
-        curr = 6
-        for row in self.grid:
-            if row[x-1] != 0:
+        curr = -1
+        # for row in self.grid:
+        #     if row[x-1] != 0:
+        #         pass
+        #         break
+        #     curr+=1
+        
+        for thing in self.grid[x-1]:
+            if thing != 0:
                 pass
                 break
-            curr = curr - 1
+            curr = curr + 1
         
         #if curr == -1, then all columns are filled, if curr == len of row then it places on the very bottom
-        if curr == 6:
+        if curr == -1:
             print("all columns are filled! Please play a different move!")
             return self.grid
         
+        print("TEST")
+        print(curr)
         newGrid = self.grid
         newGrid[x-1][curr] = self.turn
-        self.turn = (self.turn + 1)%2 + 2 #FIX THIS, SHOULD FLIP TURNS 1 = 2, 2 = 1.
+        self.turn = 3-(self.turn) # SHOULD FLIP TURNS 1 = 2, 2 = 1.
         return newGrid
     
     def updateGrid(self, grid):
@@ -34,9 +42,26 @@ class Board:
         
     def display(self):
         print("\n  1  2  3  4  5  6  7")  # column headers
-        for row in self.grid[::-1]:
+        
+        for row in range(6):
+            for col in self.grid:
+                line = ""
+                cell = col[row-1]
+                if cell == 1:
+                    line += " 🔴"
+                elif cell == 2:
+                    line += " 🟡"
+                else:
+                    line += " 🔘"
+                print(line)
+        print()  # extra newline for spacing
+
+        print("\n  1  2  3  4  5  6  7")  # column headers
+    
+        for row in range(6):  # top to bottom
             line = ""
-            for cell in row:
+            for col in range(7):  # left to right
+                cell = self.grid[col-1][row-1]
                 if cell == 1:
                     line += " 🔴"
                 elif cell == 2:
@@ -65,8 +90,11 @@ testItem = Board()
 testItem.updateGrid(testItem.placeToken(1))
 testItem.updateGrid(testItem.placeToken(1))
 testItem.updateGrid(testItem.placeToken(1))
-testItem.updateGrid(testItem.placeToken(1))
 
+
+
+
+print(testItem.grid)
 
 print(testItem.display())
 
